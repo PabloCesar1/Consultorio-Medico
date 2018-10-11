ebb6464009d8e72042a53201d767071aebc1db78
PabloEspana <pablo_cesar1995@hotmail.com>
2018-10-11 04:43:52 -0500

    Mensaje de citas médicas

--- vistas/guiListadoCitas.py
+++ vistas/guiListadoCitas.py
@@ -52,4 +52,4 @@ class listadoCitas(QtWidgets.QMainWindow, Ui_Dia):
 				self.frmCita = frmCita(self.date, self.hora) # Instancia de la clase
 				self.frmCita.show()
 		else:
-			QtWidgets.QMessageBox.information(self, 'Información', 'Por favor seleccione la hora de la ', QtWidgets.QMessageBox.Ok)
+			QtWidgets.QMessageBox.information(self, 'Información', 'Por favor seleccione la hora de la cita', QtWidgets.QMessageBox.Ok)
